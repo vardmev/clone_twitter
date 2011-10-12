5802da4a40a9a52ccfac351b32a3dd2caa285db8
dmitriy <vardmev@gmail.com>
2011-10-12 11:14:08 +0400

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 CloneTwitter::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,3 +60,4 @@ CloneTwitter::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 end
+
