b86b9ef5c9287459ece27387cd8de3f13eadebfc
dmitriy <vardmev@gmail.com>
2011-10-13 17:01:57 +0400

    Finished layout and routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 CloneTwitter::Application.routes.draw do
 
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
+
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
