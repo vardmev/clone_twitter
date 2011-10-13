1c52b252161c9888a108702dcf9679c3ddff0ce5
dmitriy <vardmev@gmail.com>
2011-10-13 16:27:36 +0400

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,9 @@ class PagesController < ApplicationController
   def about
     @title = "About"
   end
+
+  def help
+    @title = "Help"
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 CloneTwitter::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/about"
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,6 +56,7 @@ CloneTwitter::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+
 
   # See how all your routes lay out with "rake routes"
 
