f943360a3307a74fd8cf482921ebb5730ee688cf
dmitriy <vardmev@gmail.com>
2011-10-26 17:15:55 +0400

    Added ajax to follow/unfollow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,10 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
     if @user.follower_notification?
       UserMailer.send_email(@user, current_user).deliver
     end
@@ -13,7 +16,10 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
 
--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -28,6 +28,13 @@ describe RelationshipsController do
         response.should be_redirect
       end.should change(Relationship, :count).by(1)
     end
+    
+    it "should create a relationship using Ajax" do
+      lambda do
+        xhr :post, :create, :relationship => { :followed_id => @followed }
+        response.should be_success
+      end.should change(Relationship, :count).by(1)
+    end
   end
 
   describe "DELETE 'destroy'" do
@@ -43,6 +50,13 @@ describe RelationshipsController do
       lambda do
         delete :destroy, :id => @relationship
         response.should be_redirect
+      end.should change(Relationship, :count).by(-1)
+    end
+    
+    it "should destroy a relationship using Ajax" do
+      lambda do
+        xhr :delete, :destroy, :id => @relationship
+        response.should be_success
       end.should change(Relationship, :count).by(-1)
     end
   end
